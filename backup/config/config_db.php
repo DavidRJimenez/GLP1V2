58e40808093fe9bbc3f1ff0975d04787b5185953
DavidRJimenez <david.jimenezn16@gmail.com>
2025-07-23 15:34:20 -0500

    Changing things

--- backup/config/config_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('GLPI_DBHOST', 'localhost');     // Dirección del servidor de la base de datos
-define('GLPI_DBUSER', 'root');          // Usuario de la base de datos
-define('GLPI_DBPASS', '123');           // Contraseña de la base de datos
-define('GLPI_DBNAME', 'glpi');          // Nombre de la base de datos
-define('GLPI_DBPORT', '3306');          // Puerto de conexión (3306 es el predeterminado para MariaDB/MySQL)
-?>
